f7d3c469fdafb06469959238da17d2f7b4e1de58
Artem Tyutnev <artem.tiutnev@gmail.com>
2024-05-14 00:53:18 +0300

    Добавлена сущность CasinoBank

---
+++ src/Entity/CasinoBank.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CasinoBankRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CasinoBankRepository::class)]
+class CasinoBank
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'float')]
+    private float $winningPot;
+
+    #[ORM\Column(type: 'float')]
+    private float $playerLosePot;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWinningPot(): float
+    {
+        return $this->winningPot;
+    }
+
+    public function setWinningPot(float $winningPot): self
+    {
+        $this->winningPot = $winningPot;
+
+        return $this;
+    }
+
+    public function getPlayerLosePot(): float
+    {
+        return $this->playerLosePot;
+    }
+
+    public function setPlayerLosePot(float $playerLosePot): self
+    {
+        $this->playerLosePot = $playerLosePot;
+
+        return $this;
+    }
+}
